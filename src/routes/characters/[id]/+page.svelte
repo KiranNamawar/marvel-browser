85daf7635537beb2b2f42f02fe73be58923ce3a2
Kiran Namawar <kirannamawar@gmail.com>
2025-10-04 15:57:56 +0530

    refactor: use reusable components in character detail page
    - Replace inline markup with Comic, Event, Series, and Story components
    - Enable story display section (previously commented out)
    - Add debug inspection for character data
    - Improve code readability and maintainability

--- src/routes/characters/[id]/+page.svelte
+++ src/routes/characters/[id]/+page.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
 	import { getImageUrl } from '$lib/api';
+	import Comic from '$lib/components/Comic.svelte';
+	import Event from '$lib/components/Event.svelte';
+	import Series from '$lib/components/Series.svelte';
+	import Story from '$lib/components/Story.svelte';
 
 	const { data } = $props();
 	const { character, comics, series, events, stories } = data;
+
+	$inspect(character);
 </script>
 
 <h1>{character.name}</h1>
@@ -14,11 +20,7 @@
 	<div>
 		<h2>Comics</h2>
 		{#each comics as comic (comic.id)}
-			<img
-				src={getImageUrl(comic.thumbnail.path, comic.thumbnail.extension, 'portrait_medium')}
-				alt={comic.title}
-			/>
-			<!-- <p>{comic.title}</p> -->
+			<Comic {comic} />
 		{/each}
 	</div>
 {/if}
@@ -26,12 +28,8 @@
 {#if series.length !== 0}
 	<div>
 		<h2>Series</h2>
-		{#each series as serie (serie.id)}
-			<img
-				src={getImageUrl(serie.thumbnail.path, serie.thumbnail.extension, 'portrait_medium')}
-				alt={serie.title}
-			/>
-			<!-- <p>{serie.title}</p> -->
+		{#each series as s (s.id)}
+			<Series series={s} />
 		{/each}
 	</div>
 {/if}
@@ -40,26 +38,16 @@
 	<div>
 		<h2>Events</h2>
 		{#each events as event (event.id)}
-			<img
-				src={getImageUrl(event.thumbnail.path, event.thumbnail.extension, 'portrait_medium')}
-				alt={event.title}
-			/>
-			<!-- <p>{event.title}</p> -->
+			<Event {event} />
 		{/each}
 	</div>
 {/if}
 
-<!-- {#if stories.length !== 0}
+{#if stories.length !== 0}
 	<div>
 		<h2>Stories</h2>
 		{#each stories as story (story.id)}
-			{#if story.thumbnail}
-				<img
-					src={getImageUrl(story.thumbnail.path, story.thumbnail.extension, 'portrait_medium')}
-					alt={story.title}
-				/>
-			{/if}
-			<p>{story.title}</p>
+			<Story {story} />
 		{/each}
 	</div>
-{/if} -->
+{/if}
